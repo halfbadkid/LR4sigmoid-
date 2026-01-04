a35d4bacbb4e6d490fdc06fab643d85f2dbc0fa7
halfbadkid <merdekachew@gmail.com>
2026-01-04 13:53:19 +0800

    Add files via upload

---
+++ LR4-sigmoid.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+
+st.set_page_config(page_title="Sigmoid Activation Function", layout="centered")
+
+st.title("Sigmoid Activation Function Visualisation")
+st.write("### f(x) = 1 / (1 + e^(-x))")
+
+x_min, x_max = st.slider("Select x-range", -10.0, 10.0, (-6.0, 6.0))
+
+x = torch.linspace(x_min, x_max, 400)
+sigmoid = nn.Sigmoid()
+y = sigmoid(x)
+
+fig, ax = plt.subplots()
+ax.plot(x.numpy(), y.numpy())
+ax.axhline(0.5, linestyle="--")
+ax.axvline(0)
+ax.set_xlabel("Input x")
+ax.set_ylabel("Output f(x)")
+ax.set_title("Sigmoid Activation Function")
+
+st.pyplot(fig)
